938e3f2894fff3860dbdd28155db1bf3658303d7
devjwsong <devjwsong@gmail.com>
2021-12-23 02:24:13 +0000

    Modified data processing.

---
+++ exec_data_process.sh
@@ -0,0 +1,7 @@
+python src/data_process.py \
+    --seed=0 \
+    --data_dir="data" \
+    --raw_dir="raw" \
+    --save_dir="processed" \
+    --bert_type="bert-base-uncased" \
+    --train_ratio=0.8
--- src/data_process.py
+++ src/data_process.py
@@ -1,55 +1,59 @@
 from tqdm import tqdm
-from transformers import *
+from glob import glob
+from transformers import BertTokenizer
 
 import argparse
 import os
+import random
 import json
+import pickle
 
 
-def load_file(full_path, tokenizer):
+def load_file(file, tokenizer):
     total_tokens = []
     total_tags = []
     
-    with open(full_path, 'r') as f:
-        lines = f.readlines()
+    with open(file, 'r') as f:
+        data = json.load(f)
         
-    dialogue_tokens = []
-    dialogue_tags = []
-    for i, line in enumerate(tqdm(lines)):
-        if line.strip() != config['dialogue_split']:
-            comps = line.strip().split('\t')
-            speaker = comps[0]
-            sent = comps[1]
+    for dial in tqdm(data):
+        dial_tokens, dial_tags = [], []
+        turns = dial['utterances']
+        for turn in turns:
+            sp = turn['speaker']
+            text = turn['text']
             
-            tokens = tokenizer.tokenize(sent)
+            tokens = tokenizer.tokenize(text)
             entity_tags = ['O'] * len(tokens)
             
-            if len(comps) > 2:
-                entities = comps[2]
-                tokens, entity_tags = find_entities(tokens, entities, entity_tags, tokenizer)
+            if 'segments' in turn:
+                segs = turn['segments']
+                tokens, entity_tags = find_entities(tokens, segs, entity_tags, tokenizer)
             
-            dialogue_tokens.append(['[' + speaker + ']'] + tokens)
-            dialogue_tags.append(entity_tags)
-        else:
-            total_tokens.append(dialogue_tokens)
-            total_tags.append(dialogue_tags)
-            dialogue_tokens = []
-            dialogue_tags = []
+            assert len(tokens) == len(entity_tags)
+            
+            dial_tokens.append([sp] + tokens)
+            dial_tags.append(entity_tags)
+            
+        assert len(dial_tokens) == len(dial_tags)
+            
+        total_tokens.append(dial_tokens)
+        total_tags.append(dial_tags)
+    
+    assert len(total_tokens) == len(total_tags)
             
     return total_tokens, total_tags  # (N, T, L), (N, T, L)
         
         
-def find_entities(tokens, entities, entity_tags, tokenizer):
-    entity_list = entities.split(config['outer_split_symbol'])
+def find_entities(tokens, segs, entity_tags, tokenizer):
+    entity_list = [(seg['text'], seg['annotations'][0]['name']) for seg in segs]
     checked = [False] * len(tokens)
     
     for entity in entity_list:
-        entity_name = entity.split(config['inner_split_symbol'])[0]
-        entity_tag = entity.split(config['inner_split_symbol'])[1]
+        value, tag = entity
+        entity_tokens = tokenizer.tokenize(value)
 
-        entity_tokens = tokenizer.tokenize(entity_name)
-
-        entity_tags, checked = find_sublist(tokens, entity_tokens, entity_tag, entity_tags, checked)
+        entity_tags, checked = find_sublist(tokens, entity_tokens, tag, entity_tags, checked)
         
     return tokens, entity_tags
         
@@ -73,60 +77,6 @@ def find_sublist(full, sub, tag, entity_tags, checked):
                     entity_tags = [f'I-{tag}' if cur_tag == 'O' and (j>i and j<i+len(sub)) else cur_tag for j, cur_tag in enumerate(entity_tags)]
                     
     return entity_tags, checked
-            
-
-def save_text(full_dir, dialogues, dialogue_labels):
-    if not os.path.isdir(full_dir):
-        os.mkdir(full_dir)
-    
-    with open(f"{full_dir}/inputs.txt", 'w') as f:
-        for d, dialogue_tokens in enumerate(tqdm(dialogues)):
-            history = []
-            for t, tokens in enumerate(dialogue_tokens):
-                speaker = tokens[0]
-                sent = ' '.join(tokens[1:])
-                utter = f"{speaker}\t{sent}"
-                
-                if t < config['max_time']:
-                    history.append(utter)
-                else:
-                    history = history[1:] + [utter]
-                
-                f.write(f"{config['utter_split'].join(history)}\n")
-                
-    with open(f"{full_dir}/labels.txt", 'w') as f:
-        for d, dialogue_tags in enumerate(tqdm(dialogue_labels)):
-            for t, tags in enumerate(dialogue_tags):
-                label = ' '.join(tags)
-                f.write(f"{label}\n")
-                
-
-def save_ids(full_dir, dialogues, dialogue_labels):
-    if not os.path.isdir(full_dir):
-        os.mkdir(full_dir)
-    
-    with open(f"{full_dir}/inputs.ids", 'w') as f:
-        for d, dialogue_tokens in enumerate(tqdm(dialogues)):
-            history = []
-            for t, tokens in enumerate(dialogue_tokens):
-                speaker = tokens[0]
-                sent = [str(tokenizer._convert_token_to_id(token)) for token in tokens[1:]]
-                sent = ' '.join(sent)
-                utter = f"{speaker}\t{sent}"
-                
-                if t < config['max_time']:
-                    history.append(utter)
-                else:
-                    history = history[1:] + [utter]
-                
-                f.write(f"{config['utter_split'].join(history)}\n")
-
-    with open(f"{full_dir}/labels.ids", 'w') as f:
-        for d, dialogue_tags in enumerate(tqdm(dialogue_labels)):
-            for t, tags in enumerate(dialogue_tags):         
-                label = [str(class_dict[tag]) for tag in tags]
-                label = ' '.join(label)
-                f.write(f"{label}\n")
 
 
 def count_utter_num(tokens):
@@ -139,16 +89,19 @@ def count_utter_num(tokens):
     
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_path', required=True, type=str, help="The path to configuration file.")
+    parser.add_argument('--seed', default=0, type=int, help="The random seed.")
+    parser.add_argument('--data_dir', default="data", type=str, help="The parent data directory.")
+    parser.add_argument('--raw_dir', default="raw", type=str, help="The directory which contains the raw data json files.")
+    parser.add_argument('--save_dir', default="processed", type=str, help="The directory which will contain the parsed data pickle files.")
+    parser.add_argument('--bert_type', default="bert-base-uncased", type=str, help="The BERT type to load.")
+    parser.add_argument('--train_ratio', default=0.8, type=float, help="The ratio of train set to the number of each domain file.")
     
     args = parser.parse_args()
     
-    print("Loading the configurations...")
-    with open(args.config_path, 'r') as f:
-        config = json.load(f)
+    random.seed(args.seed)
     
     print("Loading the tokenizer...")
-    tokenizer = BertTokenizer.from_pretrained(config['bert_name'])
+    tokenizer = BertTokenizer.from_pretrained(args.bert_type)
     
     total_train_tokens = []
     total_train_tags = []
@@ -159,44 +112,56 @@ if __name__=='__main__':
     
     class_dict = {'O': 0}
     
-    file_list = os.listdir(f"{config['data_dir']}/{config['original_dir']}")
-    for file in file_list:
-        if os.path.isfile(f"{config['data_dir']}/{config['original_dir']}/{file}"):
-            print(f"Processing {file}...")
-            tokens, tags = load_file(f"{config['data_dir']}/{config['original_dir']}/{file}", tokenizer)
-            train_tokens, train_tags = tokens[:int(len(tokens)*config['train_frac'])], tags[:int(len(tags)*config['train_frac'])]
-            remained_tokens, remained_tags = tokens[int(len(tokens)*config['train_frac']):], tags[int(len(tags)*config['train_frac']):]
-            
-            f = config['valid_frac'] / (1.0-config['train_frac'])     
-            valid_tokens, valid_tags = remained_tokens[:int(len(remained_tokens)*f)], remained_tags[:int(len(remained_tags)*f)]
-            test_tokens, test_tags = remained_tokens[int(len(remained_tokens)*f):], remained_tags[int(len(remained_tags)*f):]
-
-            total_train_tokens += train_tokens
-            total_train_tags += train_tags
-            total_valid_tokens += valid_tokens
-            total_valid_tags += valid_tags
-            total_test_tokens += test_tokens
-            total_test_tags += test_tags
+    json_files = glob(f"{args.data_dir}/{args.raw_dir}/*.json")
+    print(json_files)
+    
+    for file in json_files:
+        print(f"Processing {file}...")
+        tokens, tags = load_file(file, tokenizer)
+        
+        pairs = list(zip(tokens, tags))
+        random.shuffle(pairs)
+        tokens, tags = list(zip(*pairs))
+        tokens, tags = list(tokens), list(tags)
+        
+        train_tokens, train_tags = tokens[:int(len(tokens)*args.train_ratio)], tags[:int(len(tags)*args.train_ratio)]
+        remained_tokens, remained_tags = tokens[int(len(tokens)*args.train_ratio):], tags[int(len(tags)*args.train_ratio):]
+        
+        valid_tokens, valid_tags = remained_tokens[:int(len(remained_tokens)*0.5)], remained_tags[:int(len(remained_tags)*0.5)]
+        test_tokens, test_tags = remained_tokens[int(len(remained_tokens)*0.5):], remained_tags[int(len(remained_tags)*0.5):]
+        
+        total_train_tokens += train_tokens
+        total_train_tags += train_tags
+        total_valid_tokens += valid_tokens
+        total_valid_tags += valid_tags
+        total_test_tokens += test_tokens
+        total_test_tags += test_tags
             
     train_utter_num = count_utter_num(total_train_tokens)
     valid_utter_num = count_utter_num(total_valid_tokens)
     test_utter_num = count_utter_num(total_test_tokens)
             
-    full_dir = f"{config['data_dir']}/{config['entity_dir']}"
-    if not os.path.isdir(full_dir):
-        os.mkdir(full_dir)
+    save_dir = f"{args.data_dir}/{args.save_dir}"
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
     
     print("Making the label dictionary...")
-    with open(f"{full_dir}/{config['tags_name']}.json", 'w') as f:
+    with open(f"{save_dir}/class_dict.json", 'w') as f:
         json.dump(class_dict, f)
         
     print("Saving each data files...")
-    save_text(f"{full_dir}/{config['train_name']}", total_train_tokens, total_train_tags)
-    save_ids(f"{full_dir}/{config['train_name']}", total_train_tokens, total_train_tags)
-    save_text(f"{full_dir}/{config['valid_name']}", total_valid_tokens, total_valid_tags)
-    save_ids(f"{full_dir}/{config['valid_name']}", total_valid_tokens, total_valid_tags)
-    save_text(f"{full_dir}/{config['test_name']}", total_test_tokens, total_test_tags)
-    save_ids(f"{full_dir}/{config['test_name']}", total_test_tokens, total_test_tags)
+    with open(f"{save_dir}/train_tokens.pkl", 'wb') as f:
+        pickle.dump(total_train_tokens, f)
+    with open(f"{save_dir}/train_tags.pkl", 'wb') as f:
+        pickle.dump(total_train_tags, f)
+    with open(f"{save_dir}/valid_tokens.pkl", 'wb') as f:
+        pickle.dump(total_valid_tokens, f)
+    with open(f"{save_dir}/valid_tags.pkl", 'wb') as f:
+        pickle.dump(total_valid_tags, f)
+    with open(f"{save_dir}/test_tokens.pkl", 'wb') as f:
+        pickle.dump(total_test_tokens, f)
+    with open(f"{save_dir}/test_tags.pkl", 'wb') as f:
+        pickle.dump(total_test_tags, f)
     
     print("Finished!")
     
